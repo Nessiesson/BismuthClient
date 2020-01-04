962cfd3405e7f7079583b59394a53b254749d369
nessie <nessie@hotmail.dk>
2020-01-04 21:07:45 +0100

    Show particles through blocks.

--- src/main/java/si/bismuth/bismuthclient/ParticleShowItems.java
+++ src/main/java/si/bismuth/bismuthclient/ParticleShowItems.java
@@ -1,6 +1,12 @@
 package si.bismuth.bismuthclient;
 
+import com.mumfrey.liteloader.gl.GL;
 import net.minecraft.client.particle.ParticleSmokeNormal;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.entity.Entity;
 import net.minecraft.world.World;
 
 public class ParticleShowItems extends ParticleSmokeNormal {
@@ -13,5 +19,18 @@ public class ParticleShowItems extends ParticleSmokeNormal {
 	public void onUpdate() {
 		super.onUpdate();
 		this.motionY = 0D;
+	}
+
+	@Override
+	public void renderParticle(BufferBuilder buffer, Entity entityIn, float partialTicks, float rotationX, float rotationZ, float rotationYZ, float rotationXY, float rotationXZ) {
+		final Tessellator tessellator = Tessellator.getInstance();
+		tessellator.draw();
+		GlStateManager.disableDepth();
+		GlStateManager.color(1F, 1F, 1F);
+		buffer.begin(GL.GL_QUADS, DefaultVertexFormats.PARTICLE_POSITION_TEX_COLOR_LMAP);
+		super.renderParticle(buffer, entityIn, partialTicks, rotationX, rotationZ, rotationYZ, rotationXY, rotationXZ);
+		tessellator.draw();
+		GlStateManager.enableDepth();
+		buffer.begin(GL.GL_QUADS, DefaultVertexFormats.PARTICLE_POSITION_TEX_COLOR_LMAP);
 	}
 }
